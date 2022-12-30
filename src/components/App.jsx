bfddbaf795805679502d04c2e2b1734f56664bf8
annamelya2021 <anna.basyuk@gmail.com>
2022-12-30 13:12:36 +0200

    ;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export const App = () => {
         stats={data}
       />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
